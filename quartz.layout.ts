0cebaa685c47f4989802490d490cb30aa55b6ec7
enzo <969611570@qq..com>
2026-03-07 17:20:42 +0800

    Quartz sync: Mar 7, 2026, 5:20 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
+      GitHub: "https://github.com/zhouenzo/eGarden.git",
       "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
